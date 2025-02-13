66d1ba4c533be12a5987a1ade82414ae9312e076
Jinane <jinanealayli@gmail.com>
2025-02-13 09:54:44 +0200

    merge

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -1,21 +1,36 @@
- 
-const mongoose = require('mongoose');
+  
+  const mongoose = require('mongoose');
 
-const PortfolioSchema = new mongoose.Schema(
-  {
-    header: { type: String },
-    description: { type: String },
-    projects: [
-      {
-        date: { type: Date },
+  const PortfolioSchema = new mongoose.Schema(
+    {
+      title: { type: String },
+      description: { type: String },
+      portfolio:{
         title: { type: String },
-        slug: { type: String },
-        imageUrl: { type: String },
-        link: { type: String }
+       projects: [
+        {
+          date: { type: Date },
+          title: { type: String },
+          slug: { type: String },
+          imageUrl: { type: String },
+          link: { type: String }
+        }
+      ]},
+      quotation:{
+        iconUrl:{ type: String },
+        quote:{type: String},
+        author:{type: String}
+      },
+      technology:{
+        title: { type: String },
+        technologies:[{
+        iconUrl:{ type: String },
+        slug:{type: String},
       }
-    ]
-  },
-  { collection: 'Portfolio' }
-);
+        ]
+      }
+    },
+    { collection: 'Portfolio' }
+  );
 
-module.exports= mongoose.models.Portfolio || mongoose.model('Portfolio', PortfolioSchema);
+  module.exports= mongoose.models.Portfolio || mongoose.model('Portfolio', PortfolioSchema);
